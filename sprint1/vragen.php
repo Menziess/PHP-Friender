107e6c465990743bd0ad3cef6a1603040b0c6ffd
RoosRiemersma <roos.riemersma@hotmail.com>
2018-01-15 11:17:31 +0100

    bla

--- sprint1/vragen.php
+++ sprint1/vragen.php
@@ -1,5 +1,41 @@
+<html>
+<body>
+
 <?php
 
-print $_POST["werken"]
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql="INSERT INTO user ('first name', 'last name', 'date of birth', 'email', 'password', 'password check')
+
+VALUES ('$_POST[fname]','$_POST[lname]')";
+
+
+if (!mysql_query($sql,$con))
+
+  {
+
+  die('Error: ' . mysql_error());
+
+  }
+
+echo "1 record added";
+
+
+
+mysql_close($con)
 
 ?>
+
+</body>
+</html>
