3f33508a509a3f2b547828dcde341ee9f2548ba9
Menziess <stefan_schenk@hotmail.com>
2018-01-15 18:07:08 +0100

    Removed segment routing.

--- app/src/view/template/nav.php
+++ app/src/view/template/nav.php
@@ -1,5 +1,7 @@
-<ul class="nav">
-	<li class="nav-item"><a href="/">Home</a></li>
-	<li class="nav-item"><a href="/user">Profile</a></li>
-	<li class="nav-item"><a href="/404">404</a></li>
-</ul>
+<nav>
+	<ul>
+		<li ><a href="/">Home</a></li>
+		<li ><a href="/user">Profile</a></li>
+		<li ><a href="/404">404</a></li>
+	</ul>
+</nav>
