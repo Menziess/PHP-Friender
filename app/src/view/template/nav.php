9c18fc2617165edb02f9595d75b5dac23a653625
RoosRiemersma <roos.riemersma@hotmail.com>
2018-01-16 16:42:13 +0100

    Added dropdown to nav.

--- app/src/view/template/nav.php
+++ app/src/view/template/nav.php
@@ -5,5 +5,15 @@
 		<li ><a href="/user">Profile</a></li>
 		<li ><a href="/404">404</a></li>
 		<li style="float:right"><a href="/login">LOGIN</a></li>
+
+		<li class="dropdown" style="float: right">
+			<a href="javascript:void(0)" class="dropbtn">Gebruiker</a>
+			<div class="dropdown-content">
+			<a href="#">Settings</a>
+			<a href="#">Foto's</a>
+			<a href="#">Events</a>
+			<a href="#">Matches</a>
+			</div>
+		</li>
 	</ul>
 </nav>
