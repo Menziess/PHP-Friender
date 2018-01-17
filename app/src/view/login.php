7d8b8b6e620a1dc8eb9bf6309dfad511428e05bc
RoosRiemersma <roos.riemersma@hotmail.com>
2018-01-17 11:12:01 +0100

    login pages fixed

--- app/src/view/login.php
+++ app/src/view/login.php
@@ -7,43 +7,10 @@
 			<input type="password" placeholder="Enter Password" name="psw" required>
 		</form>
 
-<<<<<<< HEAD
-<body>
-<!-- Header -->
-	<ul class="header">
-		<li class="nav"><a href="#logo">LOGO</a></li>
-		<li class="nav" style="float:right"><a href="signup">SIGN UP</a></li>
-	</ul>
-<!-- Container -->
-	<div class="container">
-		<div class="div_forms">
-			<div class="form-style-8">
-					<form>
-						<input type="text" placeholder="Enter Username" name="uname" required>
-						<input type="password" placeholder="Enter Password" name="psw" required>
-					</form>
-					<button class="button button--secondary">
-							<span class="button__inner"> SIGN IN!  </span>
-					</button>
-			</div>
-		</div>
-	</div>
-
-<!-- footer -->
-	<div class="footer">
-		<ul class="foot">
-				<li class="foot-item"><a href="#contact">Contact</a></li>
-				<li class="foot-item"><a href="/Applications/AMPPS/www/Webtechnieken-Voor-KI/sprint2/design/privacy.html">Privacy</a></li>
-				<li class="foot-item"><a href="#overons">Over ons</a></li>
-		</ul>
-	</div>
-</body>
-=======
 <!--
 		ZO MOET DE BUTTON WORDEN:
 -->
 		<!-- <input type="submit"> -->
->>>>>>> 69fb3a51117bc4701aaca2bd2717aac11fbf4aee
 
 		<button class="button button--secondary">
 				<span class="button__inner"> SIGN IN!  </span>
