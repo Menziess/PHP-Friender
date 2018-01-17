4a0e59509352134c291cf5c18e7e9e7afcc1bd00
Menziess <stefan_schenk@hotmail.com>
2018-01-17 22:15:00 +0100

    Greatly improved style consistency.

--- app/src/view/login.php
+++ app/src/view/login.php
@@ -1,23 +1,32 @@
 <? include 'template/head.php' ?>
 
-<div class="container" style="display:block">
-	<div class="form">
-		<form action="/login" method="POST">
-			<input type="text" placeholder="Enter Username" name="email" required>
-			<input type="password" placeholder="Enter Password" name="password" required>
-			Remember me: <input type="checkbox" name="rememberme" value="1"><br>
+<div class="container">
+	<form action="/login" method="POST">
 
-			<!--
-				ZO MOET DE BUTTON WORDEN:
-			-->
-			<input type="submit" value="Submit">
-		</form>
+		<h2>Log in</h2>
+
+		<span style="error">
+			<? echo $error ?? "" ?>
+		</span>
+
+		<input type="email" placeholder="Enter Username" name="email" required>
+		<input type="password" placeholder="Enter Password" name="password" required>
+
+		<br>
+
+		<input name="rememberme" type="checkbox"
+			checked="checked"> Remember me
+		<!--
+			ZO MOET DE BUTTON WORDEN:
+		-->
+
+		<input type="submit" value="Submit">
 
 		<!-- <button class="button button--secondary">
 				<span class="button__inner"> SIGN IN!  </span>
 		</button> -->
 
-	</div>
+	</form>
 </div>
 
 <? include 'template/tail.php' ?>
--- app/src/view/signup.php
+++ app/src/view/signup.php
@@ -1,61 +1,60 @@
 
 <? include 'template/head.php' ?>
 
-<div class="container" style="display:block">
-	<div class="form">
+<div class="container">
+
+	<form name="form" action="/user" method="POST">
+
 		<h2>Maak een account aan</h2>
 
-		<p style="background: red;">
+		<span style="error">
 			<? echo $error ?? "" ?>
+		</span>
+
+		<input name="first_name" type="text"
+			placeholder="Voornaam"
+			required>
+
+		<input name="last_name" type="text"
+			placeholder="Achternaam"
+			required>
+
+		<input name="date_of_birth" type="date"
+			placeholder="Geboorte datum"  max="2018-02-01"
+			required>
+
+		<input name="email" type="email"
+			placeholder="Email"
+			required>
+
+		<input id="password" name="password" type="password"
+			placeholder="Wachtwoord"
+			required>
+
+		<input id="password_confirm" name="password_confirm"
+			type="password"
+			placeholder="Herhaal wachtwoord"
+			required>
+
+		<br>
+
+		<input name="rememberme" type="checkbox"
+			checked="checked"> Remember me
+
+		<p>
+			By creating an account you agree to our <a href="/privacy">Terms & Privacy</a>.
 		</p>
 
-		<form name="form" action="/user" method="POST">
-
-			<input name="first_name" type="text"
-				placeholder="Voornaam"
-				required>
-
-			<input name="last_name" type="text"
-				placeholder="Achternaam"
-				required>
-
-			<input name="date_of_birth" type="date"
-				placeholder="Geboorte datum"  max="2018-02-01"
-				required>
-
-			<input name="email" type="email"
-				placeholder="Email"
-				required>
-
-			<input id="password" name="password" type="password"
-				placeholder="Wachtwoord"
-				required>
-
-			<input id="password_confirm" name="password_confirm"
-				type="password"
-				placeholder="Herhaal wachtwoord"
-				required>
-
-			<br>
-
-			<input name="remember_me" type="checkbox"
-				checked="checked"> Remember me
-
-			<p>
-				By creating an account you agree to our <a href="/privacy">Terms & Privacy</a>.
-			</p>
-
 <!--
-			De button moet een <input> element worden:
+		De button moet een <input> element worden:
 -->
-			<input type="submit" value="Submit">
+		<input type="submit" value="Submit">
 
-			<!-- <button class="button button--secondary">
-					<span class="button__inner"> Lets make friends!  </span>
-			</button> -->
+		<!-- <button class="button button--secondary">
+				<span class="button__inner"> Lets make friends!  </span>
+		</button> -->
 
-		</form>
-	</div>
+	</form>
 
 </div>
 
