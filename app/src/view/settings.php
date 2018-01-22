0797320f69d005ec66b974099133b8ea8ac77e9b
RoosRiemersma <roos.riemersma@hotmail.com>
2018-01-22 14:25:15 +0100

    settings page

--- app/src/view/settings.php
+++ app/src/view/settings.php
@@ -4,7 +4,7 @@
 
 	<form class="card fixed" action="/settings" method="POST" enctype="multipart/form-data">
 
-		<h2> Current profile picture: </h2>
+		<h2> SETTINGS: </h2>
 
 		<? if (isset($errors)): ?>
 			<ul>
@@ -16,20 +16,60 @@
 			</ul>
 		<? endif; ?>
 
-		<!-- Profile picture -->
-		<div class="center">
-			<? if (isset($picture)): ?>
-				<img src="/../../uploads/<? echo $picture->filename ?>"
-					class="profile-pic" alt="Profile picture">
-			<? else: ?>
-				<img src="/../../res/img/placeholder.jpg"
-					class="profile-pic" alt="Nog geen foto">
-			<? endif; ?>
-		</div>
+		<div class="grid">
+			<!-- Profile picture -->
+			<div style="grid-area: span 1 / span 3" class="center right">
+				<? if (isset($picture)): ?>
+					<img src="/../../uploads/<? echo $picture->filename ?>"
+						class="profile-pic" alt="Profile picture">
+				<? else: ?>
+					<img src="/../../res/img/placeholder.jpg"
+						class="profile-pic" alt="Nog geen foto">
+				<? endif; ?>
+			</div>
 
-		<input type="file" name="image"
-			accept="image/x-png,image/jpeg,image/jpg">
-		<input type="submit">
+			<div style="grid-area: span 1 / span 9" class="middle">
+				<input type="file" name="image"
+					accept="image/x-png,image/jpeg,image/jpg">
+				<div style="">
+					<input style="width:60%" type="submit">
+				</div>
+			</div>
+
+			<!-- settings form -->
+			<form>
+				<div class="center quarter right">
+					<label for="first_name">Naam</label>
+				</div>
+				<div class="half left">
+					<input type="text"  id="first_name" value="NAAM">
+				</div>
+				<br>
+				<div class="center quarter right">
+					<label for="bio">Biografie</label>
+				</div>
+				<div class="half left">
+					<input type="text"  id="bio" value="BIO">
+				</div>
+				<br>
+				<div class="center quarter right">
+					<label for="email">Email</label>
+				</div>
+				<div class="half left">
+					<input type="email"  id="email" value="EMAIL">
+				</div>
+				<br>
+				<div class="center quarter right">
+					<label for="prive">Privé account</label>
+				</div>
+				<div>
+					<input type="checkbox"  id="prive" value="on" checked="checked">
+				</div>
+				<div class="half left">
+					<label for="prive">Als je een privé account hebt kan alleen jij de inhoud van je profiel zien.</label>
+				</div>
+			</form>
+		</div>
 	</form>
 </div>
 
