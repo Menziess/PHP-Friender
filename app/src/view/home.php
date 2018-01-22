ef2a8002b2f3a59fac07403c070b7def77eeec9a
Sarah Bosscha <sarahbosscha@wcw-stud-145-109-45-10.wireless.uva.nl>
2018-01-22 11:51:05 +0100

    .

--- app/src/view/home.php
+++ app/src/view/home.php
@@ -5,26 +5,6 @@
 
 	<img class="logomain" src="../../res/img/main.png" alt="Logo">
 
-<<<<<<< HEAD
-	<div class="card">
-
-		<div class="card-header">
-			<? echo $message ?? "" ?>
-		</div>
-
-		<body1>
-		<button class="button"><a href="/signup" class="button__inner">
-		Sign up</a></button>
-
-
-
-			<button class="button button--secondary">
-				<a href="/login" class="button__inner">Sign in</a>
-			</button>
-
-
-		</body1>
-=======
 	<div class="grid">
 		<h2 class="template-default">
 			<? echo $message ?? "Welcome!" ?>
@@ -37,7 +17,6 @@
 		<button class="button button--secondary">
 			<a href="/login" class="button__inner">Sign in</a>
 		</button>
->>>>>>> cbcba765dc84c3af3e8369cfa8a520716878a0c5
 	</div>
 
 </div>
