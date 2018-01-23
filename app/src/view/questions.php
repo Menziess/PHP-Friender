2e8b75302f340366eec116bbfdac66ad227e0cef
RoosRiemersma <roos.riemersma@hotmail.com>
2018-01-23 15:12:44 +0100

    nummering toegevoegd

--- app/src/view/questions.php
+++ app/src/view/questions.php
@@ -14,7 +14,13 @@
 		<h2>Kies telkens de optie die het beste bij jou past</h2>
 
 		<? if (isset($answers)): ?>
-			<? foreach ($answers as $answer) { ?>
+			<? $i = 0;
+			 foreach ($answers as $index => $answer) {
+				if ($index % 2 == 0) {
+					$i++;
+					echo "Vraag $i: ";
+				}
+			?>
 				<p>
 					<input name="<? echo $answer->question_id ?>"
 						type="radio" value="0" required checked>
