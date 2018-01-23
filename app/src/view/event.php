bad0e63be2340501310b13c27b867a2a119feba4
Sarah Bosscha <sarahbosscha@wcw-stud-145-109-22-88.wireless.uva.nl>
2018-01-23 15:17:46 +0100

    .

--- app/src/view/event.php
+++ app/src/view/event.php
@@ -4,7 +4,7 @@
 <div class="container">
 
 <div class="grid">
-<!--
+
 	<? if (true): ?>
 		<div class="card half">
 			<h2>IT'S A MATCH! </h2>
@@ -33,7 +33,7 @@
 			<h2>Je hebt nog geen events </h2>
 			<p1>Zorg dat je leuker bent en dus een match krijgt. </p1>
 		</div>
-	<? endif; ?> -->
+	<? endif; ?>
 
 	<div class="card full">
 		JE KAN MESSENGEREN HIER
