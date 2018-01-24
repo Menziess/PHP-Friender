a0fe7a7cfca447431d34fe606fcfbc8189ce6501
RoosRiemersma <roos.riemersma@hotmail.com>
2018-01-24 13:43:20 +0100

    all users tabel

--- app/src/view/admin.php
+++ app/src/view/admin.php
@@ -9,14 +9,28 @@
 
 		<div class="full left">
 		<h2>All Users:</h2>
-			<?
-			foreach($users as $user) {
-				echo $user->email. "	" . $user->last_name ; ?>
-				<br>
-			<?
-			}
-			?>
+			<table>
+				<thead>
+					<tr>
+						<th>Last name</th>
+						<th>First name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					<!--Use a while loop to make a table row for every DB row-->
+					<?php foreach($users as $user) { ?>
+					<tr>
+						<!--Each table column is echoed in to a td cell-->
+						<td><?php echo $user->last_name; ?></td>
+						<td><?php echo $user->first_name; ?></td>
+						<td><?php echo $user->email; ?></td>
+					</tr>
+					<? } ?>
+				</tbody>
+			</table>
 		</div>
+
 	</div>
 </div>
 
