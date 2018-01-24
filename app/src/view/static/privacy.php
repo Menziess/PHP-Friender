a60246d616a000757e1d3a94cb0702515a6934fb
Sarah Bosscha <sarahbosscha@wcw-stud-145-109-33-1.wireless.uva.nl>
2018-01-24 14:55:52 +0100

    .

--- app/src/view/static/privacy.php
+++ app/src/view/static/privacy.php
@@ -1,14 +1,30 @@
 <? include __DIR__ . '/../template/head.php' ?>
 
+
+
 <div class="container">
 	<div class="grid">
 		<h2 class="full" style="color: #00ffb2; font-weight: bold;">
 		PRIVACY </h2>
-		<div class="full middle ">
-			<h4> joe, komt helemaal goed, we zorgen alleen maar dat je nieuwe vriende maakt.</h4>
+		<div class="card halfmiddle">
+
+		<div class="halfmiddle">
+			<h4>Bij Friender zijn we van mening dat privacy een basisrecht is van iedereen.</h4>
 		</div>
-		<div class="full middle ">
-			<h4> we slaan paar dingen op, maar gebeurt echt niks mee. </h4>
+
+		<div class="halfmiddle">
+			<h4> En je hebt nogal wat persoonlijke informatie op je Friender devices staan.
+			Het is je volste recht om die informatie helemaal voor jezelf te houden.
+			Zo weten wij als een van de weinig of je nou voor Thierry of voor Jesse bent, pfoeh dat zou maar uitkomen...
+			Elk Friender account is er helemaal op ingericht om die informatie af te schermen.
+			De match die voor jou wordt gemaakt zal niet in de groep vrijgeven wat jij bij je enquete hebt ingevuld; ook zullen
+			de andere users niet jou emailadres te zien krijgen, je kan alleen met elkaar praten via het event. </h4>
+		</div>
+
+		<div class="halfmiddle">
+		<h4>Keer op keer hebben we aangetoond dat een geweldige gebruikerservaring niet ten koste hoeft te gaan van je privacy en beveiliging. Integendeel. </h4>
+		</div>
+
 		</div>
 	</div>
 
