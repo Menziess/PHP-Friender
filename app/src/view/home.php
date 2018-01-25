270966b934b798773e1e397a46f096ce2c814a2e
Sarah Bosscha <sarahbosscha@wcw-stud-145-109-49-28.wireless.uva.nl>
2018-01-25 14:28:36 +0100

    .

--- app/src/view/home.php
+++ app/src/view/home.php
@@ -4,17 +4,18 @@
 <div class="container" >
 	<div class="grid overflow" >
 
-		<div class="quarter">
+		<div class="eenderde center">
 			<img class="screenshot" src="../../res/img/Screenshot.png" alt="screenshot"
+			style="width: 100%; height: auto; max-width: 18em;"
 			width="300px" height="600px" >
 		</div>
 
-		<div class="threequarter grid">
+		<div class="tweederde grid">
 			<img class="logomain full" src="../../res/img/main.png" alt="Logo">
 
 			<h2 class="full">Vind je ideale vriendengroep!</h2>
 
-			<div class="full middle">
+			<div class="full grid middle">
 				<button class="half button"
 					onclick="navigate('/login', 300);">
 					<a class="button__inner"><u>Sign in</u></a>
