5fffb9147c57565989d8ae385eb94d99d8450038
Sarah Bosscha <sarahbosscha@wcw-stud-145-109-49-28.wireless.uva.nl>
2018-01-25 16:05:50 +0100

    .

--- app/src/view/static/about.php
+++ app/src/view/static/about.php
@@ -1,14 +1,18 @@
 <? include __DIR__ . '/../template/head.php' ?>
 
-<div class="container">
-	<div class="grid">
-
-		<h2 class="full" style="color: #00ffb2; font-weight: bold;">
+<h2 class="full" style="color: #00ffb2; font-weight: bold;">
 			OVER ONS
 		</h2>
 
+
+<div class="container">
+<div class ="card">
+	<div class="grid">
+
+
+
 		<? foreach ($devs as $dev) { ?>
-			<div class="quarter middle ">
+			<div class="quarter middle">
 				<b><? echo $dev->first_name ?></b>
 				<br>
 				<? echo $dev->bio ?>
@@ -28,5 +32,6 @@
 		<? } ?>
 	</div>
 </div>
+		</div>
 
 <? include __DIR__ . '/../template/tail.php' ?>
