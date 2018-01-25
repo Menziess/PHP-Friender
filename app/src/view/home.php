abf85314f2541db17d3ff6af03301f2209930971
Sarah Bosscha <sarahbosscha@wcw-stud-145-109-49-28.wireless.uva.nl>
2018-01-25 13:19:11 +0100

    .

--- app/src/view/home.php
+++ app/src/view/home.php
@@ -2,23 +2,29 @@
 <? require 'template/head.php' ?>
 
 <div class="container" >
-
-	<img class="logomain" src="../../res/img/main.png" alt="Logo">
-
 	<div class="grid overflow" >
 
-		<h2 class="full">Vind je ideale vriendengroep!</h2>
+		<div class="quarter">
+			<img class="screenshot" src="../../res/img/Screenshot.png" alt="screenshot"
+			width="300px" height="600px" >
+		</div>
 
-		<div class="halfmiddle">
-		<button class="half button"
-			onclick="navigate('/login', 300);">
-			<a class="button__inner"><u>Sign in</u></a>
-		</button>
+		<div class="threequarter grid">
+			<img class="logomain full" src="../../res/img/main.png" alt="Logo">
 
-		<button class="half button button--secondary"
-			onclick="navigate('/signup', 300);">
-			<a class="button__inner"><u>Sign up</u></a>
-		</button>
+			<h2 class="full">Vind je ideale vriendengroep!</h2>
+
+			<div class="full middle">
+				<button class="half button"
+					onclick="navigate('/login', 300);">
+					<a class="button__inner"><u>Sign in</u></a>
+				</button>
+
+				<button class="half button button--secondary"
+					onclick="navigate('/signup', 300);">
+					<a class="button__inner"><u>Sign up</u></a>
+				</button>
+			</div>
 		</div>
 
 	</div>
