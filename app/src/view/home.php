9433a91e2b340b22975308d4d6f9e4739b7bf12e
Menziess <stefan_schenk@hotmail.com>
2018-01-26 14:46:31 +0100

    Fixed homepage.

--- app/src/view/home.php
+++ app/src/view/home.php
@@ -14,17 +14,19 @@
 			<h2 class="full">Vind je ideale vriendengroep!</h2>
 
 			<? if (!isset($_SESSION['first_name'])): ?>
-			<div class="full grid middle">
-
-				<button class="half button button--secondary"
-					onclick="navigate('/signup', 300);">
-					<a class="button__inner"><u>Sign up</u></a>
-				</button>
-				<button class="half button"
-					onclick="navigate('/login', 300);">
-					<a class="button__inner"><u>Sign in</u></a>
-				</button>
-
+			<div class="full grid middle overflow">
+				<div class="half center">
+					<button class="button button--secondary"
+						onclick="navigate('/signup', 300);">
+						<a class="button__inner"><u>Sign up</u></a>
+					</button>
+				</div>
+				<div class="half center">
+					<button class="button"
+						onclick="navigate('/login', 300);">
+						<a class="button__inner"><u>Sign in</u></a>
+					</button>
+				</div>
 			</div>
 			<? endif; ?>
 
