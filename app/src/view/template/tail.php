f696dddfe17736335de7e3b3f566741157caa098
Menziess <stefan_schenk@hotmail.com>
2018-01-26 22:31:16 +0100

    Fixed stray footer tag.

--- app/src/view/template/tail.php
+++ app/src/view/template/tail.php
@@ -1,10 +1,11 @@
-</body>
 
 	<? include 'footer.php' ?>
 
 	<script src="../../res/js/jquery.min.js"></script>
 	<script src="../../res/js/jquery.validate.min.js"></script>
 	<script src="../../res/js/script.js"></script>
+
+</body>
 
 </html>
 
