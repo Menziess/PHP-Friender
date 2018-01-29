61d80ee44670f8854efcfe331d177c866d6986f9
Menziess <stefan_schenk@hotmail.com>
2018-01-29 11:58:27 +0100

    Removed duplicate.

--- app/src/model/User.php
+++ app/src/model/User.php
@@ -50,6 +50,9 @@ class User extends Model {
 		exit;
 	}
 
+	/**
+	 * @todo Roos
+	 */
 	public function permit(int $id)
 	{
 		$potentialHacker = $this->auth();
@@ -58,17 +61,6 @@ class User extends Model {
 		// en niet dit, dan error
 		Router::error(401);
 		exit;
-	}
-
-	/**
-	 * @todo Roos
-	 */
-	public function permit(int $id)
-	{
-		// $id moet gelijk zijn aan id van ingelogde user
-		// of user moet admin zijn
-		// anders moet een 401 error getoont worden
-		return true;
 	}
 
 	/**
