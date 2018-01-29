b3ee7c739d953d642a6a5541676f12cf5bed39e2
RoosRiemersma <roos.riemersma@hotmail.com>
2018-01-29 12:14:00 +0100

    permit user

--- app/src/model/User.php
+++ app/src/model/User.php
@@ -53,23 +53,25 @@ class User extends Model {
 	public function permit(int $id)
 	{
 		$potentialHacker = $this->auth();
-		//  als niet dit
-		$potentialHacker->is_admin;
-		// en niet dit, dan error
-		Router::error(401);
-		exit;
+		$hacker_id = $potentialHacker->id;
+
+		if ($hacker_id !== $id && !$potentialHacker->is_admin){
+			Router::error(401);
+			exit;
+		}
+		return true;
 	}
 
 	/**
 	 * @todo Roos
 	 */
-	public function permit(int $id)
-	{
-		// $id moet gelijk zijn aan id van ingelogde user
-		// of user moet admin zijn
-		// anders moet een 401 error getoont worden
-		return true;
-	}
+	// public function permit(int $id)
+	// {
+	// 	// $id moet gelijk zijn aan id van ingelogde user
+	// 	// of user moet admin zijn
+	// 	// anders moet een 401 error getoont worden
+	// 	return true;
+	// }
 
 	/**
 	 * Checks if user is authenticated.
