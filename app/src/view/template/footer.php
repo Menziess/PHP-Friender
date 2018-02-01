82234d3e04c8b3628b299ce45f354d611898e75d
Sarah Bosscha <sarahbosscha@wcw-stud-145-109-0-188.wireless.uva.nl>
2018-02-01 15:04:08 +0100

    .

--- app/src/view/template/footer.php
+++ app/src/view/template/footer.php
@@ -3,7 +3,6 @@
 	<ul>
 		<li><a href="/contact">Contact</a></li>
 		<li><a href="/privacy">Privacy</a></li>
-		<li><a href="/about">Over ons</a></li>
-		<li><a href="/concept">Concept</a></li>
+		<li><a href="/about">Concept</a></li>
 	</ul>
 </footer>
