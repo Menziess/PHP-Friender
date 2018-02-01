543bb33167331a6a3a97de3550db1d3974b2db16
Menziess <stefan_schenk@hotmail.com>
2018-02-01 20:48:39 +0100

    Fixed login page.

--- app/src/view/login.php
+++ app/src/view/login.php
@@ -1,42 +1,36 @@
 <? include 'template/head.php' ?>
 
-<form class="card full middle" action="/login" method="POST">
+<div class="full middle">
+	<form class="card full middle" action="/login" method="POST">
 
-	<h2>Log in</h2>
+		<h2>Log in</h2>
 
-	<input type="email" placeholder="Uw email" name="email"
-	value="<? echo $email ?? "" ?>" required>
-	<input type="password" placeholder="Uw wachtwoord" name="password" required>
+		<input type="email" placeholder="Uw email" name="email"
+		value="<? echo $email ?? "" ?>" required>
+		<input type="password" placeholder="Uw wachtwoord" name="password" required>
 
-	<br>
-
-	<input name="rememberme" type="checkbox"
-		checked="checked"> Onthoud mijn gegevens
-	<br>
-	<br>
-	<div>
-		<input type="submit" value="Log in">
-	</div>
-
-	<br>
-	<br>
-	<div class="full center">
-		Of maak nu snel een account
 		<br>
-		als je er nog geen hebt!
-		<a href="/signup" class=" full middle">
-			<input type="button" value="Sign up">
-		</a>
-	</div>
-</form>
-<!-- <br>
-<div class="card full center" style="text-align:center">
-	Of maak nu snel een account
+
+		<input name="rememberme" type="checkbox"
+			checked="checked"> Onthoud mijn gegevens
+		<br>
+		<br>
+		<div>
+			<input type="submit" value="Log in">
+		</div>
+	</form>
 	<br>
-	als je er nog geen hebt!
-	<a href="/signup" class=" full middle">
-		<input type="button" value="Sign up">
-	</a>
-</div> -->
+
+	<div class="card full middle">
+		<div class=" center">
+			Of maak nu snel een account
+			<br>
+			als je er nog geen hebt!
+			<a href="/signup">
+				<input type="button" value="Sign up">
+			</a>
+		</div>
+	</div>
+</div>
 
 <? include 'template/tail.php' ?>
